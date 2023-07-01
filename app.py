1391cc4ec584b064887805e585ed6c12a399001b
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-07-01 12:20:34 +0200

    web application deployment

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
 from crypt import methods
-from flask import Flask
-from src.logger import logging
+from flask import Flask, render_template, request, jsonify
+from src.pipeline.prediction_pipeline import CustomClass, PredictionPipeline
+
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    
-    logging.info('we are testing our second methods of logging')
-    
-    return "Welcome to Engineering wala bhaiyaa"
 
+@app.route('/', methods = ["GET", "POST"])
+def prediction_data():
+    if request.method == "GET":
+        return render_template('home.html')
+    
+    else:
+        data = CustomClass(
+    
+            age = int(request.form.get("age")),
+            workclass = int(request.form.get("workclass")),
+            education_num = int(request.form.get("education_num")),
+            marital_status = int(request.form.get("marital_status")),
+            occupation = int(request.form.get("occupation")),
+            relationship = int(request.form.get("relationship")),
+            race = int(request.form.get("race")),
+            sex = int(request.form.get("sex")),
+            capital_gain = int(request.form.get("capital_gain")),
+            capital_loss = int(request.form.get("capital_loss")),
+            hours_per_week = int(request.form.get("hours_per_week")),
+            native_country = int(request.form.get("native_country"))
 
+        )
+        
+    final_data = data.get_data_dataFrame()
+    prediction_pipeline = PredictionPipeline()
+    pred = prediction_pipeline.predict(final_data)
+    
+    result = pred
+    
+    if result == 0:
+        return render_template("results.html", final_result = "Your Yearly Income is Less than Equal to 50k:{}".format(result) )
+    
+    elif result == 1:
+        return render_template(("results.html", final_result = "Your Yearly Income is More than 50k:{}".format(result) ))
+    
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(host = "0.0.0.0", debug = True)
+
+    
+    
+    
